74a0c2ddb8e9f2c701667258b3e93ecc4872e2a7
Mounia Belkheir <mouniabelkheir@outlook.com>
2023-09-10 10:57:23 +0100

    completed assignment

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
+// Function to check if any pair of numbers in the array adds up to the target number
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  
+  // Initialize a new Set to store the numbers we've seen so far
+  let numSet = new Set();
+
+  // Loop through each number in the array
+  for (let i = 0; i < array.length; i++) {
+
+      // Calculate the complement of the current number (i.e., target - current number)
+      let complement = target - array[i];
+
+      // If the complement is in our Set, we've found a pair that adds up to the target
+      if (numSet.has(complement)) {
+          return true;  // Return true and end the function
+      }
+
+      // If not, add the current number to our Set and continue with the next iteration
+      numSet.add(array[i]);
+  }
+
+  // If we've gone through the entire array and haven't found a pair, return false
+  return false;
 }
 
 /* 
-  Write the Big O time complexity of your function here
-*/
+This function uses a Set to keep track of numbers we’ve seen so far. For each number, it calculates the complement (target - current number) and checks if it’s in the Set. If it is, that means we’ve found a pair that adds up to the target, so we return true. If we go through the entire array without finding a pair, we return false.
 
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
+This solution has a time complexity of O(n), where n is the length of the array, because we’re doing a single pass through the array. It has a space complexity of O(n) as well, because in the worst case scenario, we might have to store every number in the Set. This is an optimized solution as it avoids the need for nested loops which would result in a higher time complexity of O(n^2).
 */
 
 // You can run `node index.js` to view these console logs
